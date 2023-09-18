b39cf752c6aff7378a4e083e5778a5028f01c8d4
Arjun <arjunsuresh1987@gmail.com>
2023-09-18 15:53:41 +0530

    Support SS, Server and MS comparisons

--- qa-compare-results.php
+++ qa-compare-results.php
@@ -118,185 +118,126 @@ class qa_compare_results
 			$sysversion1 = "v3.0";
 			$sysversion2 = "v3.1";
 		}
-		$scenario="offline";
-
-		$query = "select * from ^mlcommons_inference_results where platform = '$system1' and scenario='$scenario' and version='$sysversion1' $modelfilterstring"; //and division='$division' and mlperfmodel='$model' and systemtype like '%$category%'";
-		$raw_result = qa_db_query_sub($query);
-		$result1 = qa_db_read_all_assoc($raw_result);
-
-		$query = "select * from ^mlcommons_inference_results where platform = '$system2' and scenario='$scenario' and version='$sysversion2' $modelfilterstring"; //and division='$division' and mlperfmodel='$model' and systemtype like '%$category%'";
-		$raw_result = qa_db_query_sub($query);
-		$result2 = qa_db_read_all_assoc($raw_result);
-
-		$is_power = ($result2[0]['power_result'] != 0);
-		if(!$is_power)
-		{
-			$power_string = "false";
-		}
-		else {
-			$power_string = "true";
-		}
-		// = $result2[0]['power_result'] != 0;
-
-
-		$data1 = "$sysversion1: $system1";
-		$data2 = "$sysversion2: $system2";
+		$scenarios  = array(
+			"Offline",
+			"Server",
+			"SingleStream",
+			"MultiStream"
+		);
+		$ytitle_scenarios = array(
+			"Offline" => "Samples per Second",
+			"Server" => "Samples per Second",
+			"SingleStream" => "Latency per sample in milliseconds",
+			"MultiStream" => "Latency per query of 8 samples in milliseconds",
+		);
 		$qa_content['custom_0'] =  "
 			<script type='text/javascript'>
-var data1 = '$data1', data2 = '$data2', draw_power = $power_string, draw_power_efficiency = $power_string, sortcolumnindex=4, perfsortorder=1;
+var data1 = {}, data2 = {}, draw_power = {}, draw_power_efficiency = {}, ytitle = {}, sortcolumnindex, perfsortorder;
+</script>
+";
+		$customid = 1;
+		foreach ($scenarios as $scenario) {
+
+			$query = "select * from ^mlcommons_inference_results where platform = '$system1' and scenario='$scenario' and version='$sysversion1' $modelfilterstring"; //and division='$division' and mlperfmodel='$model' and systemtype like '%$category%'";
+			$raw_result = qa_db_query_sub($query);
+			$result1 = qa_db_read_all_assoc($raw_result);
+			if(count($result1) == 0) continue;
+			$query = "select * from ^mlcommons_inference_results where platform = '$system2' and scenario='$scenario' and version='$sysversion2' $modelfilterstring"; //and division='$division' and mlperfmodel='$model' and systemtype like '%$category%'";
+			$raw_result = qa_db_query_sub($query);
+			$result2 = qa_db_read_all_assoc($raw_result);
+			if(count($result2) == 0) continue;
+
+			$is_power = ($result2[0]['power_result'] != 0);
+			if(!$is_power)
+			{
+				$power_string = "false";
+			}
+			else {
+				$power_string = "true";
+			}
+			// = $result2[0]['power_result'] != 0;
+
+
+			$data1 = "$sysversion1: $system1";
+			$data2 = "$sysversion2: $system2";
+			$ytitle = $ytitle_scenarios[$scenario];
+			$qa_content['custom_'.$customid] =  "
+<script type='text/javascript'>
+data1['$scenario'] = '$data1', data2['$scenario'] = '$data2', draw_power['$scenario'] = $power_string, draw_power_efficiency['$scenario'] = $power_string,
+ytitle['$scenario'] = '$ytitle',
+sortcolumnindex = 4, perfsortorder = 1;
 </script>";
 
-		//print_r($result2);
-		$models = array();
-		echo count($result1);
-		for($i = 0; $i < count($result1); $i++) {
-			$row = $result1[$i];
-			if(!in_array($row['model'], $models)) {
-				//array_push($models, $row['model']);
+			//print_r($result2);
+			$models = array();
+			$result2_models = array();
+			$result_scenarios = array();
+			for($i = 0; $i < count($result2); $i++) {
+				$row = $result2[$i];
+				if(!in_array($row['model'], $result2_models)) {
+					array_push($result2_models, $row['model']);
+				}
 			}
-		}
-		for($i = 0; $i < count($result2); $i++) {
-			$row = $result2[$i];
-			if(!in_array($row['model'], $models)) {
-				array_push($models, $row['model']);
+			for($i = 0; $i < count($result1); $i++) {
+				$row = $result1[$i];
+				if(!in_array($row['model'], $models) and in_array($row['model'], $result2_models)) {
+					array_push($models, $row['model']);
+				}
 			}
-		}
-		//print_r($models);
-		//print_r($result1);
-		$results1 = array();
-		$results2 = array();
-		foreach($models as $model)
-		{
-			foreach($result1 as $row) {
+			//print_r($models);
+			//print_r($result1);
+			$results1 = array();
+			$results2 = array();
+			foreach($models as $model) {
+			    foreach($result1 as $row) {
 				if($row['model'] == $model)
-					$results1[$model] = $row;
-			}
-			foreach($result2 as $row) {
+				    $results1[$model] = $row;
+			    }
+			    foreach($result2 as $row) {
 				if($row['model'] == $model)
 					$results2[$model] = $row;
+			    }
 			}
-		}
-		$html = '		<div class="pager"> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/first.png" class="first"/> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/prev.png" class="prev"/> 
-			<span class="pagedisplay"></span> <!-- this can be any element, including an input --> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/next.png" class="next"/> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/last.png" class="last"/> 
-			<select class="pagesize" title="Select page size"> 
-			<option selected="selected" value="10">10</option> 
-			<option value="20">20</option> 
-			<option value="30">30</option> 
-			<option value="all">All</option> 
-			</select>
-			<select class="gotoPage" title="Select page number"></select>
-			</div>
-';
+			$tableposthtml =       	'<!-- pager --> 
+			    <div class="pager"> 
+			    <img src="https://mottie.github.io/tablesorter/addons/pager/icons/first.png" class="first"/> 
+			    <img src="https://mottie.github.io/tablesorter/addons/pager/icons/prev.png" class="prev"/> 
+			    <span class="pagedisplay"></span> <!-- this can be any element, including an input --> 
+			    <img src="https://mottie.github.io/tablesorter/addons/pager/icons/next.png" class="next"/> 
+			    <img src="https://mottie.github.io/tablesorter/addons/pager/icons/last.png" class="last"/> 
+			    <select class="pagesize" title="Select page size"> 
+			    <option selected="selected" value="10">10</option> 
+			    <option value="20">20</option> 
+			    <option value="30">30</option> 
+			    <option value="all">All</option> 
+			    </select>
+			    <select class="gotoPage" title="Select page number"></select>
+			    </div>';
+			$html = "<h2>Comparing $scenario scenario for $data1 and $data2 </h2>".$tableposhtml;
 
 
-		$html .= "<table class=\"tablesorter\" id=\"results\">";
-		$html .= "<thead> <tr>";
-		$tableheader = "
+			$htmltable  = $this -> construct_table($scenario, $models, $data1, $data2, $is_power, $results1, $results2);
+			$html .= $htmltable;
+			$html .= $tableposthtml;
+			$qa_content['custom_'.$customid] .= $html;
 
-			<th>Model</th>
-			<th>$data1</th>
-			<th>$data2</th>
-			<th>Performance Delta</th>";
+			$resultjson = json_encode($result);
 
-		if($is_power) {
-			$tableheader .= "
-			<th>Power 1</th>
-			<th>Power 2</th>
-			<th>Power Delta</th>
-			<th>Power Efficiency 1</th>
-			<th>Power Efficiency 2</th>
-			<th>Power Efficiency Delta</th>";
-		}
-		$tableheader .="
-			</tr>";
-		$html .= $tableheader."
-			</thead>";
-		$html .= "<tfoot> <tr>".$tableheader."
-			</tr>
-			</tfoot>";
-
-		$performance_title = "Samples per Second";
-		foreach($models as $row) {
-			$html.= "<tr>";
-			$html .= "<td class='model'>".$row  ."</td>";
-			$perf1 = $results1[$row]['performance_result'];
-			$perf2 = $results2[$row]['performance_result'];
-			if($perf2)
-				$perfdelta = round(((1-$perf1/ $perf2)), 4) * 100;
-				$html .= "<td>". $perf1. "</td>";
-				$html .= "<td>". $perf2. "</td>";
-				$html .= "<td>". $perfdelta. "</td>";
+			$qa_content['custom_'.$customid] .= '
+			    <div id="chartContainer'.$scenario.'1" class="bgtext" style="height: 370px; width: 100%;"></div>
+			    <button class="btn btn-primary"  id="printChart'.$scenario.'1">Download</button>';
 			if($is_power) {
-				$pow1 = $results1[$row]['power_result'];
-				$pow2 = $results2[$row]['power_result'];
-				if($pow2)
-					$powdelta = round(((1-$pow1/ $pow2)), 4) * 100;
-				if($pow1)
-					$peff1 = round($perf1/$pow1, 5);
-				else
-					$peff1 = "";	
-				if($pow2)
-					$peff2 = round($perf2/$pow2, 5);
-				else
-					$peff2 = "";	
-				$html .= "<td>". $pow1. "</td>";
-				$html .= "<td>". $pow2. "</td>";
-				$html .= "<td>". $powdelta. "</td>";
+			    $qa_content['custom_'.$customid] .= '
 
-				$html .= "<td>". $peff1. "</td>";
-				$html .= "<td>". $peff2. "</td>";
-				if($peff2)
-					$peffdelta = round(((1-$peff1/ $peff2)),4) * 100;
-				else $peff2="";
-				$html .= "<td>". $peffdelta. "</td>";
-			}
-			$html .= "</tr>";
-
-		}
-		$html .= "</table>";
-
-		$html .= '<!-- pager --> 
-			<div class="pager"> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/first.png" class="first"/> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/prev.png" class="prev"/> 
-			<span class="pagedisplay"></span> <!-- this can be any element, including an input --> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/next.png" class="next"/> 
-			<img src="https://mottie.github.io/tablesorter/addons/pager/icons/last.png" class="last"/> 
-			<select class="pagesize" title="Select page size"> 
-			<option selected="selected" value="10">10</option> 
-			<option value="20">20</option> 
-			<option value="30">30</option> 
-			<option value="all">All</option> 
-			</select>
-			<select class="gotoPage" title="Select page number"></select>
-			</div>';
-		$qa_content['custom_100'] = $html;
-
-		$resultjson = json_encode($result);
-		$html2 = '<div id="chartContainer" style="height: 370px; width: 100%;"></div>';
-		$html2 .= '<div id="chartContainer1" class="bgtext" style="height: 370px; width: 100%;"></div>';
-		$html2 .= '<div id="chartContainer2" class="bgtext" style="height: 370px; width: 100%;"></div>';
-		$html2 .= '<div id="chartContainer3" class="bgtext" style="height: 370px; width: 100%;"></div>';
-
-		$qa_content['custom_101'] = '
-			<div id="chartContainer1" class="bgtext" style="height: 370px; width: 100%;"></div>
-			<button class="btn btn-primary"  id="printChart1">Download</button>';
-		if($is_power) {
-		$qa_content['custom_101'] .= '
-
-			<div id="chartContainer2" class="bgtext" style="height: 370px; width: 100%;"></div>
-			<button class="btn btn-primary"  id="printChart2">Download</button>
-			<div id="chartContainer3" class="bgtext" style="height: 370px; width: 100%;"></div>
-			<button class="btn btn-primary"  id="printChart3">Download</button>
+				<div id="chartContainer'.$scenario.'2" class="bgtext" style="height: 370px; width: 100%;"></div>
+				<button class="btn btn-primary"  id="printChart'.$scenario.'2">Download</button>
+				<div id="chartContainer'.$scenario.'3" class="bgtext" style="height: 370px; width: 100%;"></div>
+				<button class="btn btn-primary"  id="printChart'.$scenario.'3">Download</button>
 
 ';
+			}
+			$customid++;
 		}
-
-		//$qa_content['custom_9'] = $html2;
 
 		$user_level = qa_get_logged_in_level();
 		$ok = null;
@@ -365,5 +306,73 @@ var data1 = '$data1', data2 = '$data2', draw_power = $power_string, draw_power_e
 		);
 
 		return $qa_content;
+	}
+	function construct_table($scenario, $models, $data1, $data2, $is_power, $results1, $results2) {
+		$html = "<table class=\"tablesorter\" id=\"results_$scenario\">";
+		$html .= "<thead> <tr>";
+		$tableheader = "
+
+			<th>Model</th>
+			<th>$data1</th>
+			<th>$data2</th>
+			<th>Performance Delta</th>";
+
+		if($is_power) {
+			$tableheader .= "
+			<th>Power 1</th>
+			<th>Power 2</th>
+			<th>Power Delta</th>
+			<th>Power Efficiency 1</th>
+			<th>Power Efficiency 2</th>
+			<th>Power Efficiency Delta</th>";
+		}
+		$tableheader .="
+			</tr>";
+		$html .= $tableheader."
+			</thead>";
+		$html .= "<tfoot> <tr>".$tableheader."
+			</tr>
+			</tfoot>";
+
+		$performance_title = "Samples per Second";
+		foreach($models as $row) {
+			$html.= "<tr>";
+			$html .= "<td class='model'>".$row  ."</td>";
+			$perf1 = $results1[$row]['performance_result'];
+			$perf2 = $results2[$row]['performance_result'];
+			if($perf2)
+				$perfdelta = round(((1-$perf1/ $perf2)), 4) * 100;
+				$html .= "<td>". $perf1. "</td>";
+				$html .= "<td>". $perf2. "</td>";
+				$html .= "<td>". $perfdelta. "</td>";
+			if($is_power) {
+				$pow1 = $results1[$row]['power_result'];
+				$pow2 = $results2[$row]['power_result'];
+				if($pow2)
+					$powdelta = round(((1-$pow1/ $pow2)), 4) * 100;
+				if($pow1)
+					$peff1 = round($perf1/$pow1, 5);
+				else
+					$peff1 = "";	
+				if($pow2)
+					$peff2 = round($perf2/$pow2, 5);
+				else
+					$peff2 = "";	
+				$html .= "<td>". $pow1. "</td>";
+				$html .= "<td>". $pow2. "</td>";
+				$html .= "<td>". $powdelta. "</td>";
+
+				$html .= "<td>". $peff1. "</td>";
+				$html .= "<td>". $peff2. "</td>";
+				if($peff2)
+					$peffdelta = round(((1-$peff1/ $peff2)),4) * 100;
+				else $peff2="";
+				$html .= "<td>". $peffdelta. "</td>";
+			}
+			$html .= "</tr>";
+
+		}
+		$html .= "</table>";
+		return $html;
 	}
 }
